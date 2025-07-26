3cf8ac817c908026f127232ac37a2c850f04784b
Farhan Ishrak Sadekin <farhanishrak061@gmail.com>
2025-07-26 22:09:51 +0600

    Array Searching.cpp

--- Cpp-Codes/Array Searching.cpp
+++ Cpp-Codes/Array Searching.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     int n,i;
+    int flag = 0;
 
     cout<<"Enter the searching value: ";
     cin>>n;
@@ -13,10 +14,18 @@ int main()
     {
         if(n == arr[i])
         {
+            flag = 1;
             break;
         }
     }
-    cout<<n<<" found in "<<i<<"th array index";
+    if(flag == 1)
+    {
+        cout<<n<<" found in "<<i<<"th array index";
+    }
+    else
+    {
+        cout<<"Not Found";
+    }
 
     return 0;
 }
