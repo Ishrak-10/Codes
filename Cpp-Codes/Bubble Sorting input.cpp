e525dcfe504ef185ed60f7a181ad99cd824c9f26
Farhan Ishrak Sadekin <farhanishrak061@gmail.com>
2025-07-27 20:17:50 +0600

    Update Bubble Sorting input.cpp

--- Cpp-Codes/Bubble Sorting input.cpp
+++ Cpp-Codes/Bubble Sorting input.cpp
@@ -21,15 +21,15 @@ int main()
     }
     cout<<endl;
 
-    for(int step=0; step<n; step++)
+    for(int i=0; i<n-1; i++)
     {
-        for(int i=0; i<n-1; i++)
+        for(int j=0; j<n-i-1; j++)
         {
-            if(arr[i] > arr[i+1])
+            if(arr[j] > arr[j+1])
             {
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
