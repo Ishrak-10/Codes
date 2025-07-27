e29667f69188c8ef8692d394dc9773de360c019b
Farhan Ishrak Sadekin <farhanishrak061@gmail.com>
2025-07-27 20:18:42 +0600

    Add files via upload

---
+++ Cpp-Codes/Bubble Sorting Character.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[10] = {'a','j','d','e','h','b','i','c','f','g'};
+    int n = 10;
+
+    cout<<"Before Sorting: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j] < arr[j+1])   // Descending Order
+            {
+                char temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout<<"After Sorting: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
