8bf017998606d29ee80600d2b7304c46cc17224d
Farhan Ishrak Sadekin <farhanishrak061@gmail.com>
2025-08-08 21:39:16 +0600

    Update Selection Sort.cpp

--- Cpp-Codes/Selection Sort.cpp
+++ Cpp-Codes/Selection Sort.cpp
@@ -1,39 +1,45 @@
 #include<iostream>
 using namespace std;
 
-int main()
+void SelectionSort(int arr[], int n)
 {
-    int a[] = {5,7,2,4,9};
-    int n = sizeof(a)/sizeof(a[0]);
-
-    cout<<"Before Sorting: ";
-    for(int i=0; i<n; i++)
-    {
-        cout<<a[i]<<" ";
-    }
-
     for(int i=0; i<n-1; i++)
     {
         int minIndexValue = i;
         for(int j=i+1; j<n; j++)
         {
-            if(a[j] < a[minIndexValue])
+            if(arr[j] < arr[minIndexValue])
             {
                 minIndexValue = j;
             }
         }
-        int temp = a[minIndexValue];
-        a[minIndexValue] = a[i];
-        a[i] = temp;
+        int temp = arr[minIndexValue];
+        arr[minIndexValue] = arr[i];
+        arr[i] = temp;
     }
+}
 
+int main()
+{
+    int arr[] = {5,7,2,4,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Before Sorting: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     cout<<endl;
+
+    SelectionSort(arr,n);
+
     cout<<"After Sorting: ";
     for(int i=0; i<n; i++)
     {
-        cout<<a[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 
     return 0;
 }
+
 
