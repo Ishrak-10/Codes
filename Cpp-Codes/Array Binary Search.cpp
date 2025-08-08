bb4cd4f00bcaca8e200dd79a40ca5237cde38429
Farhan Ishrak Sadekin <farhanishrak061@gmail.com>
2025-08-08 20:58:23 +0600

    Update Array Binary Search.cpp

--- Cpp-Codes/Array Binary Search.cpp
+++ Cpp-Codes/Array Binary Search.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(arr)/sizeof(arr[0]);
     int a;
     int start = 0;
@@ -42,3 +42,4 @@ int main()
 
     return 0;
 }
+
