1a5393e0ccbc549e7b53454cfb52dfa42c01feb6
Farhan Ishrak Sadekin <196329612+Ishrak-10@users.noreply.github.com>
2026-03-28 21:48:31 +0600

    Add files via upload

---
+++ C#-Codes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            char z;
+
+            Console.Write("Please Enter the First Number: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please Enter the second Number: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please Enter the Desire Operator: ");
+            z = Convert.ToChar(Console.ReadLine());
+
+            if(z == '+')
+            {
+                Console.WriteLine($"Answer = {x + y}");
+            }
+
+            else if (z == '-')
+            {
+                Console.WriteLine($"Answer = {x - y}");
+            }
+
+            else if (z == '*')
+            {
+                Console.WriteLine($"Answer = {x * y}");
+            }
+
+            else if (z == '/')
+            {
+                Console.WriteLine($"Answer = {x / y}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Oparetor");
+            }
+        }
+    }
+}
